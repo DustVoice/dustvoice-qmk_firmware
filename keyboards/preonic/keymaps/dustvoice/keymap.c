38be5f8e9556ac74e6f8def508f7cd0af46aa79b
DustVoice <info@dustvoice.de>
2021-07-27 15:08:55 +0200

    Upon further reconsideration, add other keyboard layouts too

--- keyboards/preonic/keymaps/dustvoice/keymap.c
+++ keyboards/preonic/keymaps/dustvoice/keymap.c
@@ -1,11 +1,25 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
+#include "print.h"
+
+enum layer_names
+{
+    QTY, // Default [Q]WER[[TY] layer
+    CMK, // [C]ole[m]a[k] layer
+    CDH, // [C]olemak-[DH] layer
+    WKM, // [W]or[km]an layer
+    NAL, // [N]ot [A] [L]ayer; Transparent layer for switching between keyboard layouts, by setting QTY/CDH/WKM as the default layer and TO()-ing to NAL
+    FN, // [F]unctio[n] [L]ayer (hold), with special characters, arrow keys, F-keys and further layer switching
+    MF, // [M]ouse layer, with [F]1-F15 keys
+    NUM, // [Num]pad layer
+    ADM, // [Adm]in layer with keyboard layout switching
+    MFN // [M]irror of [F]unctio[n] Layer (permanent), to serve as a function layer for other layers
+};
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 {
-    // Default QWERTY layer
-    [0] = LAYOUT_preonic_grid(
+    [QTY] = LAYOUT_preonic_grid(
     //┌────────┬────────┬────────┬────────┬────────┬────────┐┌────────┬────────┬────────┬────────┬────────┬────────┐
        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -15,12 +29,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-       KC_LCTL, KC_LGUI, KC_LCTL, KC_LALT, MO(3),   KC_SPC,   KC_ENT,  MO(3),   KC_RALT, KC_RCTL, KC_APP,  KC_RCTL
+       KC_LCTL, KC_LGUI, KC_LCTL, KC_LALT, MO(FN),  KC_SPC,   KC_ENT,  MO(FN),  KC_RALT, KC_RCTL, KC_APP,  KC_RCTL
     //└────────┴────────┴────────┴────────┴────────┴────────┘└────────┴────────┴────────┴────────┴────────┴────────┘
     ),
 
-    // Default Workman layer
-    [1] = LAYOUT_preonic_grid(
+    [CMK] = LAYOUT_preonic_grid(
+    //┌────────┬────────┬────────┬────────┬────────┬────────┐┌────────┬────────┬────────┬────────┬────────┬────────┐
+       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
+       KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
+    //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
+       KC_CAPS, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,     KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+    //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
+       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
+       KC_LCTL, KC_LGUI, KC_LCTL, KC_LALT, MO(FN),  KC_SPC,   KC_ENT,  MO(FN),  KC_RALT, KC_RCTL, KC_APP,  KC_RCTL
+    //└────────┴────────┴────────┴────────┴────────┴────────┘└────────┴────────┴────────┴────────┴────────┴────────┘
+    ),
+
+    [CDH] = LAYOUT_preonic_grid(
+    //┌────────┬────────┬────────┬────────┬────────┬────────┐┌────────┬────────┬────────┬────────┬────────┬────────┐
+       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
+       KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
+    //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
+       KC_CAPS, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,     KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+    //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
+       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,     KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
+       KC_LCTL, KC_LGUI, KC_LCTL, KC_LALT, MO(FN),  KC_SPC,   KC_ENT,  MO(FN),  KC_RALT, KC_RCTL, KC_APP,  KC_RCTL
+    //└────────┴────────┴────────┴────────┴────────┴────────┘└────────┴────────┴────────┴────────┴────────┴────────┘
+    ),
+
+    [WKM] = LAYOUT_preonic_grid(
     //┌────────┬────────┬────────┬────────┬────────┬────────┐┌────────┬────────┬────────┬────────┬────────┬────────┐
        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -30,13 +71,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
        KC_LSFT, KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,     KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-       KC_LCTL, KC_LGUI, KC_LCTL, KC_LALT, MO(3),   KC_SPC,   KC_ENT,  MO(3),   KC_RALT, KC_RCTL, KC_APP,  KC_RCTL
+       KC_LCTL, KC_LGUI, KC_LCTL, KC_LALT, MO(FN),  KC_SPC,   KC_ENT,  MO(FN),  KC_RALT, KC_RCTL, KC_APP,  KC_RCTL
     //└────────┴────────┴────────┴────────┴────────┴────────┘└────────┴────────┴────────┴────────┴────────┴────────┘
     ),
 
-    // Transparent layer for switching between keyboard layouts,
-    // by setting 0/1 as the default layer and TO'ing to this layer
-    [2] = LAYOUT_preonic_grid(
+    [NAL] = LAYOUT_preonic_grid(
     //┌────────┬────────┬────────┬────────┬────────┬────────┐┌────────┬────────┬────────┬────────┬────────┬────────┐
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -50,12 +89,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     //└────────┴────────┴────────┴────────┴────────┴────────┘└────────┴────────┴────────┴────────┴────────┴────────┘
     ),
 
-    // Function layer (hold), with special characters, arrow keys, F-keys and further layer switching
-    [3] = LAYOUT_preonic_grid(
+    [FN] = LAYOUT_preonic_grid(
     //┌────────┬────────┬────────┬────────┬────────┬────────┐┌────────┬────────┬────────┬────────┬────────┬────────┐
        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_MINS, KC_EQL,
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-       KC_TAB,  KC_F10,  TO(6),   TO(5),   TO(8),   TO(7),    TO(7),   TO(8),   TO(5),   TO(6),   KC_LBRC, KC_RBRC,
+       KC_TAB,  KC_F10,  TO(NUM), TO(MF),  TO(MFN), TO(ADM),  TO(ADM), TO(MFN), TO(MF),  TO(NUM), KC_LBRC, KC_RBRC,
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
        KC_F13,  KC_F11,  KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_F12,  KC_DEL,
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -65,8 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     //└────────┴────────┴────────┴────────┴────────┴────────┘└────────┴────────┴────────┴────────┴────────┴────────┘
     ),
 
-    // Mouse layer, with F1-F15 keys
-    [5] = LAYOUT_preonic_grid(
+    [MF] = LAYOUT_preonic_grid(
     //┌────────┬────────┬────────┬────────┬────────┬────────┐┌────────┬────────┬────────┬────────┬────────┬────────┐
        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC,
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -76,12 +113,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
        KC_LSFT, SH_TG,   KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R,  KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, SH_TG,   KC_RSFT,
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-       KC_LCTL, KC_LGUI, KC_BTN3, KC_BTN2, MO(8),   KC_BTN1,  KC_BTN1, MO(8),   KC_BTN2, KC_BTN3, KC_APP,  KC_RCTL
+       KC_LCTL, KC_LGUI, KC_BTN3, KC_BTN2, MO(MFN), KC_BTN1,  KC_BTN1, MO(MFN), KC_BTN2, KC_BTN3, KC_APP,  KC_RCTL
     //└────────┴────────┴────────┴────────┴────────┴────────┘└────────┴────────┴────────┴────────┴────────┴────────┘
     ),
 
-    // Numpad layer
-    [6] = LAYOUT_preonic_grid(
+    [NUM] = LAYOUT_preonic_grid(
     //┌────────┬────────┬────────┬────────┬────────┬────────┐┌────────┬────────┬────────┬────────┬────────┬────────┐
        KC_ESC,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, KC_NO,   KC_BSPC,
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -91,31 +127,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
        KC_LSFT, SH_TG,   KC_HOME, KC_PGUP, KC_PGDN, KC_END,   KC_P1,   KC_P2,   KC_P3,   KC_ENT,  SH_TG,   KC_RSFT,
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-       KC_LCTL, KC_LGUI, KC_LCTL, KC_LALT, MO(8),   KC_SPC,   KC_ENT,  MO(8),   KC_RALT, KC_RCTL, KC_APP,  KC_RCTL
+       KC_LCTL, KC_LGUI, KC_LCTL, KC_LALT, MO(MFN), KC_SPC,   KC_ENT,  MO(MFN), KC_RALT, KC_RCTL, KC_APP,  KC_RCTL
     //└────────┴────────┴────────┴────────┴────────┴────────┘└────────┴────────┴────────┴────────┴────────┴────────┘
     ),
 
-    // Admin layer with keyboard layout switching
-    [7] = LAYOUT_preonic_grid(
+    [ADM] = LAYOUT_preonic_grid(
     //┌────────┬────────┬────────┬────────┬────────┬────────┐┌────────┬────────┬────────┬────────┬────────┬────────┐
-       KC_ESC,  NK_OFF,  KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   NK_OFF,   KC_BSPC,
+       KC_ESC,  NK_OFF,  KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   NK_OFF,  KC_BSPC,
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-       KC_TAB,  KC_NO,   KC_NO,   DF(1),   DF(0),   KC_NO,    KC_NO,   DF(0),   DF(1),   KC_NO,   KC_NO,   KC_NO,
+       KC_TAB,  DF(WKM), DF(CMK), DF(CDH), DF(QTY), KC_NO,    KC_NO,   DF(QTY), DF(CDH), DF(CMK), DF(WKM), KC_NO,
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
        RESET,   EEP_RST, RGB_VAI, RGB_SAI, RGB_HUI, RGB_TOG,  RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, EEP_RST, RESET,
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
        KC_LSFT, SH_TG,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   SH_TG,   KC_RSFT,
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-       KC_LCTL, KC_LGUI, KC_LCTL, KC_LALT, MO(8),   KC_SPC,   KC_ENT,  MO(8),   KC_RALT, KC_RCTL, KC_APP,  KC_RCTL
+       KC_LCTL, KC_LGUI, KC_LCTL, KC_LALT, MO(MFN), KC_SPC,   KC_ENT,  MO(MFN), KC_RALT, KC_RCTL, KC_APP,  KC_RCTL
     //└────────┴────────┴────────┴────────┴────────┴────────┘└────────┴────────┴────────┴────────┴────────┴────────┘
     ),
 
-    // Mirror of function layer (toggle), to serve as a function layer for other layers
-    [8] = LAYOUT_preonic_grid(
+    [MFN] = LAYOUT_preonic_grid(
     //┌────────┬────────┬────────┬────────┬────────┬────────┐┌────────┬────────┬────────┬────────┬────────┬────────┐
        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_MINS, KC_EQL,
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-       KC_TAB,  KC_F10,  TO(6),   TO(5),   TO(2),   TO(7),    TO(7),   TO(2),   TO(5),   TO(6),   KC_LBRC, KC_RBRC,
+       KC_TAB,  KC_F10,  TO(NUM), TO(MF),  TO(NAL), TO(ADM),  TO(ADM), TO(NAL), TO(MF),  TO(NUM), KC_LBRC, KC_RBRC,
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
        KC_F13,  KC_F11,  KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_F12,  KC_DEL,
     //├────────┼────────┼────────┼────────┼────────┼────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -147,6 +181,7 @@ void matrix_init_user() {
 void set_led_depend_state(uint8_t layer) {
     val = rgblight_get_val();
 
+    uprintf("Switching to layer %d\n", layer);
     switch (layer) {
         // case 0:
         //     rgblight_mode(1);
